71c9e926873a13b3b38e1d2f78ca1a8f26938115
maxkuzzmenko <127974451+maxkuzzmenko@users.noreply.github.com>
2026-01-09 19:23:17 +0100

    more api key testing

--- create-dataset/server.js
+++ create-dataset/server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+API_KEY="EYZFUEKJFAOUIIDQBUSS"
+
 // Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(__dirname));
